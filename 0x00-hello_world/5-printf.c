91d6535192cd28dcf7e091a749421ce8e728b9dc
Alpha1444 <muteeuishola@gmail.com>
2022-10-13 21:01:27 -0700

    my co commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main-run the code in print f
+ *
+ * Return (0): Success
+ */
+int main (void)
+{
+	peintf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
