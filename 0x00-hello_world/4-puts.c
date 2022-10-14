009d6c995b0d8912c538bc605df9f8721cdd70e9
Alpha1444 <muteeuishola@gmail.com>
2022-10-13 20:24:22 -0700

    my just commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main-run the main code
+ *
+ * Return (0) (Success)
+ */
+int main (void)
+{
+puts("/"programming is likebuilding a multilingual puzzle "):
+	return (0)
+}
+
