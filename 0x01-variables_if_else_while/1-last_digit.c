df9cd38b1b707c843dbf51183ce0ef1b082db988
Alpha1444 <muteeuishola@gmail.com>
2022-10-15 10:19:53 -0700

    done

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,29 +1,28 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers go there */
 
-/* betty style doc for function main go there */
 /**
-* main - determines whether randomly generated number is even or odd
-*
-* Return (0) Success
-*/
+ * main - print last digit of random generated number
+ *
+ * Return:(0) Success
+ */
 int main(void)
 {
 	int n;
+	int j;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there then */
-	if (n > o)
-	printf("'%d' is positive\n", n" );
-	else if (n==0)
-	printf("'%d' is zero \n", n' );
-	else if (n<0)
-	printf("'%d' is negative \n", n' );
-	return (0);
+	j = n % 10;
+	if  (j > 5)
+	printf("Last digit of %d is %d and is greater than 5\n", n, j);
+	else if (j == 0)
+		printf("Last digit of %d is %d and is 0\n", n, j);
+	else if (j < 6 && j != 0)
+		printf("Last digit of '%d' is '%d' and is less than 6 and not 0\n", n, j);
+                printf("Last digit of '%d' is less than 6 and not 0\n" , n, j);
 
+	return (i0)
 
-}
-
+ }
