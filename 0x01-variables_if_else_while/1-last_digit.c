3dcd3b63ba11933bebb663279e68f2c4371718c2
Alpha1444 <muteeuishola@gmail.com>
2022-10-15 04:30:04 -0700

    my code commit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers go there */
+
+/* betty style doc for function main go there */
+/**
+* main - determines whether randomly generated number is even or odd
+*
+* Return (0) Success
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there then */
+	if (n > o)
+	printf("'%d' is positive\n", n");
+	else if (n==0)
+	printf("'%d' is zero \n", n);
+	else if (n<0)
+	printf("'%d' is negative \n", n);
+	return (0);
+}
+
